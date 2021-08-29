4e0bd05275e7cf5ed1317e03c6c0b4ac7eb31f13
Jacob Pernell <jacob.pernell@gmail.com>
2021-08-28 23:08:02 -0700

    Remove array to simplify state management, add X delete button and very basic style formatting

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-let todoArr = [];
-const form = document.querySelector('.form');
-const textInput = document.querySelector('#todo-add');
-const todoList = document.querySelector('.todo-list');
-textInput.focus();
-
-function addTodoItem(e) {
-    e.preventDefault();
-    todoArr.push(textInput.value);
-    // console.log(todoArr);
-
-    const newTodo = document.createElement('li');
-    newTodo.classList.add('todo-item');
-    newTodo.innerText = textInput.value;
-    newTodo.addEventListener('click', deleteTodoItem);
-    todoList.appendChild(newTodo);
-
-    textInput.value = '';
+(function () {
+    const form = document.querySelector('.form');
+    const textInput = document.querySelector('#todo-add');
+    const todoList = document.querySelector('.todo-list');
     textInput.focus();
-}
 
-function deleteTodoItem(e) {
-    todoArr = todoArr.filter(item => {
-        return item !== e.target.innerText;
-    });
+    function addTodoItem(e) {
+        e.preventDefault();
 
-    todoList.removeChild(e.target);
-}
+        const newTodoDiv = document.createElement('div');
+        newTodoDiv.classList.add('todo-item-container');
 
-form.addEventListener('submit', addTodoItem);
+        const newTodo = document.createElement('li');
+        newTodo.classList.add('todo-item');
+        newTodo.innerText = textInput.value;
+
+        const todoDeleteBtn = document.createElement('button');
+        todoDeleteBtn.classList.add('todo-delete-btn');
+        todoDeleteBtn.innerText = 'X';
+        todoDeleteBtn.addEventListener('click', deleteTodoItem);
+
+        newTodoDiv.appendChild(newTodo);
+        newTodoDiv.appendChild(todoDeleteBtn);
+        todoList.appendChild(newTodoDiv);
+
+        textInput.value = '';
+        textInput.focus();
+    }
+
+    function deleteTodoItem(e) {
+        todoList.removeChild(e.target.parentElement);
+    }
+
+    form.addEventListener('submit', addTodoItem);
+})();
